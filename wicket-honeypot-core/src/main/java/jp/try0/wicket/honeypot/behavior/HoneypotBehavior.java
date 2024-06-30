4a4ec078962859609391c681c374620033dba7ac
Ryo <try0.development@gmail.com>
2024-06-30 22:30:51 +0900

    Remove css

--- wicket-honeypot-core/src/main/java/jp/try0/wicket/honeypot/behavior/HoneypotBehavior.java
+++ wicket-honeypot-core/src/main/java/jp/try0/wicket/honeypot/behavior/HoneypotBehavior.java
@@ -2,7 +2,6 @@ package jp.try0.wicket.honeypot.behavior;
 
 import org.apache.wicket.Component;
 import org.apache.wicket.behavior.Behavior;
-import org.apache.wicket.markup.head.CssReferenceHeaderItem;
 import org.apache.wicket.markup.head.IHeaderResponse;
 import org.apache.wicket.markup.head.JavaScriptHeaderItem;
 import org.apache.wicket.markup.html.form.Form;
@@ -10,7 +9,6 @@ import org.apache.wicket.markup.html.form.FormComponent;
 import org.apache.wicket.markup.html.form.validation.AbstractFormValidator;
 import org.apache.wicket.model.Model;
 import org.apache.wicket.request.IRequestParameters;
-import org.apache.wicket.request.resource.CssResourceReference;
 import org.apache.wicket.resource.TextTemplateResourceReference;
 import org.apache.wicket.util.string.StringValue;
 
@@ -25,12 +23,6 @@ public class HoneypotBehavior extends Behavior {
 	 * default js reference
 	 */
 	public final static TextTemplateResourceReference JS_NO_DELAY_REFERENCE;
-
-	/**
-	 * css reference
-	 */
-	public final static CssResourceReference CSS_REFERENCE = new CssResourceReference(HoneypotBehavior.class,
-			HoneypotBehavior.class.getSimpleName() + ".css");
 
 	static {
 		// create default instance		
@@ -131,8 +123,6 @@ public class HoneypotBehavior extends Behavior {
 					HoneypotBehavior.class.getSimpleName() + ".js", "text/javascript", Model.ofMap(config.asMap()));
 			response.render(JavaScriptHeaderItem.forReference(jsReference));
 		}
-
-		response.render(CssReferenceHeaderItem.forReference(CSS_REFERENCE));
 	}
 
 	/**
